ed42cfc6a96e1f932b224cbca78e5f5107df6894
givenmphiwe <giggslive52@gmail.com>
2022-09-13 08:22:10 -0700

    lower-case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - chech for lower case letter
+ * @c : character to check the case
+ * Return: 0 or 1
+ */
+int _islower(int alpha)
+{
+	return (alpha >= 97 && alpha <= 122);
+}
