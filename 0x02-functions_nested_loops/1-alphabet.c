38c1d12695e9bb1830a81291cc23f50472b7f5e5
givenmphiwe <giggslive52@gmail.com>
2022-09-13 04:53:49 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
