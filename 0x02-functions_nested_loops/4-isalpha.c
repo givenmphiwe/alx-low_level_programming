8fbcad481afe28aaaf7d031c5f4e361d98168d85
givenmphiwe <giggslive52@gmail.com>
2022-09-13 08:36:08 -0700

    isapha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for case
+ * @alpha : variable to check
+ * Return: 0 or 1
+ */
+int _isalpha(int alpha)
+{
+	return ((alpha >= 97 && alpha <= 122 || (alpha >= 65 && alpha <= 90));
+}
+
