c06bbc72c38bbb7c8f796ab7bc3be65d068375ff
givenmphiwe <giggslive52@gmail.com>
2022-09-15 08:01:14 -0700

    multiply

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * mul - check the code
+ * @a: is integer to take num
+ * @b: is integer to take num
+ * Return: ALways 0 or 1
+ */
 int mul(int a, int b)
 {
 	return (a * b);
