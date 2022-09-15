305ab0a59aa5ae31f34e997011eb75ff62c5bc73
givenmphiwe <giggslive52@gmail.com>
2022-09-15 15:29:39 -0700

    bugs

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,6 +4,7 @@
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -17,8 +18,8 @@ int main(void)
 	 * putchar(i);
 	 * }
 	 */
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
-
