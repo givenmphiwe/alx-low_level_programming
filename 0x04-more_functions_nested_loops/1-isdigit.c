54e13ecbd4c8eeb796e0aba0fafcc9cdb3097f87
givenmphiwe <giggslive52@gmail.com>
2022-09-15 07:52:50 -0700

    is-digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the code
+ * @c: is the variable name
+ * Return: 0 or 1 Always (success)
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
