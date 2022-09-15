4c175826d43fb887160c0407efc71c7679bdd837
givenmphiwe <giggslive52@gmail.com>
2022-09-15 04:25:33 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - check for upper case letter
+ * @c: character to check the case
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the charater c to stdout
+ * @c: The character to print
+ *
+ * Return: Always 0 (Success)
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
