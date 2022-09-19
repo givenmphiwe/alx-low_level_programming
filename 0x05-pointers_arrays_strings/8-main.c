dff93ec09391e1dd2c690acfe75dae2b869d10ea
givenmphiwe <giggslive52@gmail.com>
2022-09-19 07:57:50 -0700

    array

---
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int array[5];
+
+	array[0] = 98;
+	array[1] = 402;
+	array[2] = -198;
+	array[3] = 298;
+	array[4] = -1025;
+	print_array(array, 5);
+	return (0);
+}
