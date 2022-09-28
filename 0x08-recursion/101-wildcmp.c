66f3294abf79da5b539d74a8f14571d1b9a4e7ab
givenmphiwe <giggslive52@gmail.com>
2022-09-28 05:09:26 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -40,12 +40,10 @@ int checkLast(char *s, int i)
  */
 int checker(char *s1, char *s2, int a, int b, int wildUsed)
 {
-
 	if (s1[a] != '\0')
 	{
 		if (s2[b] == '\0')
 		return (0);
-		
 		else if (s2[b] == '*')
 		{
 			if (s2[b + 1] == '*')
@@ -59,16 +57,11 @@ int checker(char *s1, char *s2, int a, int b, int wildUsed)
 		}
 		else if ((s1[a] == s2[b]) || (s2[b] == '*' && s2[b + 1] == s1[a + 1]))
 			return (checker(s1, s2, a + 1, b + 1, wildUsed));
-		
 		if (wildUsed == -1)
 			return (0);
-		
 		return (checker(s1, s2, a, wildUsed, wildUsed));
 	}
-	
 	if (s2[b] != '\0')
 		return (checkLast(s2, b));
-	
 	return (1);
 }
-
