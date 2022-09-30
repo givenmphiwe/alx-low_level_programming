787adcabe72cacfb2713719f882e6147d89a6905
givenmphiwe <giggslive52@gmail.com>
2022-09-30 07:48:51 -0700

    argv argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of number
+ * @argc: int
+ * @argv: list
+ * Return: 0.
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void) argv;
+	printf("%i\n", argc - 1);
+	return (0);
+}
