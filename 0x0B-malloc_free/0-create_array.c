d4c22b826ca510ebccc20d954af7ee0fffe34a46
givenmphiwe <giggslive52@gmail.com>
2022-10-04 01:00:04 -0700

    array-maloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * *create_array - create an array
+ * @size: size of the array
+ * @c: char to initialize
+ * Return: pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *m = malloc(size);
+
+	if (size == 0 || m == 0)
+		return (0);
+
+	while (size--)
+		m[size] = c;
+
+	return (m);
+}
