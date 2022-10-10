33268133b4285d68195bea9b4873e1fe7eb54782
givenmphiwe <giggslive52@gmail.com>
2022-10-10 04:15:59 -0700

    main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define FILE_NAME__FILE__
+
+/**
+ * main -prints the name of the file
+ *
+ * Return:Always success
+ */
+
+int main(void)
+{
+	printf("%s\n", FILE_NAME);
+	return (0);
+}
